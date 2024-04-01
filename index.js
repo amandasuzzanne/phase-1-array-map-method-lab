2ece1b0a6ded84de92532d4035753689f124c5e7
amandasuzzanne <amandasuzanne12@gmail.com>
2024-04-01 22:15:40 +0300

    Complete array map method lab

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((sentences)=>{
+    let words = sentences.split(" ");
+    let capLetters = words.map(
+      (word)=>{ 
+        return word.charAt(0).toUpperCase() + word.slice(1)
+     }
+    )
+    let response = capLetters.join(" ")
+    return response;
+  })
 }
